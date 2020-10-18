f477d9ab66ec101cf7acbff5e45ac3f3038ab6aa
EK <ekim0480@gmail.com>
2020-10-17 23:19:32 -0500

    Updates

---
+++ precache-manifest.1e38ac636e9b04c9371a2195d2500f11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a927f2eb484b58201b550e53b66ad0b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "bf580432985c80b95cb5",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "191c79b86a767d8ffe5a",
+    "url": "/react-portfolio/static/css/main.dab0b267.chunk.css"
+  },
+  {
+    "revision": "bf580432985c80b95cb5",
+    "url": "/react-portfolio/static/js/2.2ae59be1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.2ae59be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191c79b86a767d8ffe5a",
+    "url": "/react-portfolio/static/js/main.02a1391a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "18b0659e1ad4dcf50dead9bd5fcd71ef",
+    "url": "/react-portfolio/static/media/bodybg.18b0659e.jpg"
+  }
+]);
